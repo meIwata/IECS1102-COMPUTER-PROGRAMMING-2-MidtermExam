f80fb3cda086ef52cc8e85d2cd22318593fff649
waleilulu <waleilulu@gmail.com>
2025-05-29 15:41:10 +0800

    「字串處理」與「基本流程控制（for、if-else）」

--- src/Greeting.java
+++ src/Greeting.java
@@ -5,14 +5,34 @@ public class Greeting {
         System.out.print("請使用者輸入姓名: ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        String result = name.toUpperCase().charAt(0) + name.substring(1, name.length());
-        for (int i = 1; i < result.length(); i++) {
-            if (result.charAt(i) == 'A' || result.charAt(i) == 'a') {
 
+        String input = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+
+        // 預設一個空字串
+        String result = "";
+
+        // for 迴圈逐一檢查每一個字元
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            /*如果字元等於題目指定的某個字母，就把對應的數字串接到 result。如果不是，就把原本的字元串接到 result。*/
+            if (ch == 'A' || ch == 'a') {
+                result += "4";
+            } else if (ch == 'E' || ch == 'e') {
+                result += "3";
+            } else if (ch == 'I' || ch == 'i') {
+                result += "1";
+            } else if (ch == 'O' || ch == 'o') {
+                result += "0";
+            } else if (ch == 'S' || ch == 's') {
+                result += "5";
+            } else if (ch == 'T' || ch == 't') {
+                result += "7";
+            } else {
+                result += ch;
             }
         }
+
         System.out.println(result);
-
-
     }
 }
